dc0315940bdfaa25a50bd83b01e005006c312056
SankaralingamS777 <jeyaramssankaralingam@gmail.com>
2026-03-11 11:40:30 +0530

    Add sum_of_two_numbers.py to calculate sum

---
+++ sum.py
@@ -0,0 +1,11 @@
+# sum_of_two_numbers.py
+
+# Ask the user for input
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the sum
+sum_result = num1 + num2
+
+# Display the result
+print("The sum is:", sum_result)
